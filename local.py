1a33c76e1db5aae550a8d199e564e841e4dbe861
Weipeng Zhang <wp.zhang@hotmail.com>
2023-12-10 15:53:43 -0500

    Add support for command line arguments and config file

--- local.py
+++ local.py
@@ -4,53 +4,55 @@ import pandas as pd
 
 from src.features.feature_engineering import aggregate_data
 from src.models.local_trainer import LocalTrainer
+from src.utils import setup_logger
+import argparse
+from box import Box
 
 warnings.filterwarnings("ignore")
 
-DATA_PATH = Path("./data")
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", type=str, default="./data")
+    parser.add_argument("--model", type=str, default="LR")
+    parser.add_argument("--config", type=str, default="./configs/local.yaml")
+    args = parser.parse_args()
+    logger = setup_logger("Local")
 
+    # * Load Data
+    DATA_PATH = Path(args.data_dir)
+    train = pd.read_csv(DATA_PATH / "raw" / "train_data.csv", nrows=100000)
+    target = pd.read_csv(DATA_PATH / "raw" / "train_labels.csv")
+    test = pd.read_csv(DATA_PATH / "raw" / "test_data.csv", nrows=100000)
 
-# * Load Data
-train = pd.read_csv(DATA_PATH / "raw" / "train_data.csv", nrows=100000)
-target = pd.read_csv(DATA_PATH / "raw" / "train_labels.csv")
-test = pd.read_csv(DATA_PATH / "raw" / "test_data.csv", nrows=100000)
+    train.drop(columns=["S_2", "D_63", "D_64"], inplace=True)
+    test.drop(columns=["S_2", "D_63", "D_64"], inplace=True)
 
-train.drop(columns=["S_2", "D_63", "D_64"], inplace=True)
-test.drop(columns=["S_2", "D_63", "D_64"], inplace=True)
-print(train.info())
+    # * Feature Engineering
+    train = aggregate_data(train)
+    test = aggregate_data(test)
+    train["customer_ID"] = train.index
+    test["customer_ID"] = test.index
+    train = train.merge(target, on="customer_ID", how="left")
 
-# * Feature Engineering
-train = aggregate_data(train)
-test = aggregate_data(test)
+    feats = [
+        col
+        for col in train.columns
+        if col not in ["customer_ID", "target", "S_2", "D_63", "D_64"]
+    ]
 
-train["customer_ID"] = train.index
-test["customer_ID"] = test.index
-train = train.merge(target, on="customer_ID", how="left")
+    # * Fill NaN
+    train = train.fillna(-1)
+    test = test.fillna(-1)
 
-feats = [
-    col
-    for col in train.columns
-    if col not in ["customer_ID", "target", "S_2", "D_63", "D_64"]
-]
+    # * Train Model
+    model_config = Box.from_yaml(filename=args.config).get(args.model)
+    logger.info(f"Model Config: {model_config}")
 
-# * Fill NaN
-train = train.fillna(-1)
-test = test.fillna(-1)
+    lr_trainer = LocalTrainer()
+    oof_pred = lr_trainer.kfold_train(
+        train_df=train, feats=feats, target="target", **model_config.to_dict()
+    )
 
-# * Train Logistic Regression
-lr_trainer = LocalTrainer()
-oof_pred = lr_trainer.kfold_train(
-    model_name="LR",
-    model_params={"C": 0.9},
-    train_df=train,
-    feats=feats,
-    target="target",
-    n_splits=5,
-    random_state=2023,
-    normalize=True,
-)
-
-# * Make Prediction
-test_pred = lr_trainer.predict(test)
-
-print(test_pred.head())
+    # * Make Prediction
+    test_pred = lr_trainer.predict(test)
+    logger.info(test_pred.head())
--- parallel.py
+++ parallel.py
@@ -2,11 +2,10 @@ from pathlib import Path
 import warnings
 import pyspark
 import pandas as pd
-
+import argparse
+from box import Box
 
 warnings.filterwarnings("ignore")
-
-DATA_PATH = Path("./data")
 
 spark = (
     pyspark.sql.SparkSession.builder.appName("AMEX")
@@ -17,43 +16,49 @@ spark = (
 )
 spark.sparkContext.setLogLevel("ERROR")
 
-# * Load Data
-
-#train = pd.read_csv(DATA_PATH / "raw" / "train_data.csv", nrows=1000)
-target = pd.read_csv(DATA_PATH / "raw" / "train_labels.csv")
-train = spark.read.parquet(DATA_PATH/ "raw" / "train.parquet") #removed noise
-test = spark.read.parquet(DATA_PATH/ "raw" / "test.parquet")
-
-train = train.merge(target, on="customer_ID", how="left")
-
 from src.features.parallel_feature_engineering import aggregate_data_spark
-
-#train = aggregate_data_spark(train)
-
-feats = [
-    col
-    for col in train.columns
-    if col not in ["customer_ID", "target", "S_2", "D_63", "D_64"]
-]
-
-train = train.fillna(-1)
-test = test.fillna(-1)
-
-# * Train Logistic Regression
 from src.models.parallel_trainer import ParallelTrainer
+from src.utils import setup_logger
 
-plr_trainer = ParallelTrainer(spark)
-oof_pred = plr_trainer.kfold_train(
-    model_name="LR",
-    model_params={"regParam": 0.1},
-    train_df=train,
-    feats=feats,
-    target="target",
-    n_splits=5,
-    random_state=2023,
-    normalize=True,
-)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_dir", type=str, default="./data")
+    parser.add_argument("--model", type=str, default="LR")
+    parser.add_argument("--config", type=str, default="./configs/parallel.yaml")
+    args = parser.parse_args()
+    logger = setup_logger("Parallel")
 
-test_pred = plr_trainer.predict(test)
+    # * Load Data
+    DATA_PATH = Path(args.data_dir)
+    # train = pd.read_csv(DATA_PATH / "raw" / "train_data.csv", nrows=1000)
+    target = pd.read_csv(DATA_PATH / "raw" / "train_labels.csv")
+    train = spark.read.parquet(DATA_PATH / "raw" / "train.parquet")  # removed noise
+    test = spark.read.parquet(DATA_PATH / "raw" / "test.parquet")
 
-print(test_pred.head())
+    train = train.merge(target, on="customer_ID", how="left")
+
+    # train = aggregate_data_spark(train)
+
+    feats = [
+        col
+        for col in train.columns
+        if col not in ["customer_ID", "target", "S_2", "D_63", "D_64"]
+    ]
+
+    train = train.fillna(-1)
+    test = test.fillna(-1)
+
+    # * Train Model
+    model_config = Box.from_yaml(filename=args.config).get(args.model)
+    logger.info(f"Model Config: {model_config}")
+
+    plr_trainer = ParallelTrainer(spark)
+    oof_pred = plr_trainer.kfold_train(
+        train_df=train,
+        feats=feats,
+        target="target",
+        **model_config.to_dict(),
+    )
+
+    test_pred = plr_trainer.predict(test)
+    logger.info(test_pred.head())
